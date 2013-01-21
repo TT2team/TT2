7db6c46a01d0bd969ed0b460d0e60968367caa68
Jan Glagolev <jansglagolevs@gmail.com>
2013-01-22 00:07:07 +0200

    poslednije izmenenija

--- fuel/app/views/admin/index.php
+++ fuel/app/views/admin/index.php
@@ -2,6 +2,42 @@
 
 echo '<h2>Admin home page</h2>';
 
+if(isset($darbiba))
+{
+    
+    echo '<table class="table table-hover">';
+    echo '<thead>';
+        echo '<tr>';
+        echo '<td>';
+        echo 'action_id';
+        echo '</td>';
+        echo '<td>';        
+        echo 'changed_code';        
+        echo '</td>';
+        echo '<td>';
+        echo 'chenged_name';
+        echo '</td>';
+        echo '</tr>';
+    echo '</thead>';
+    echo '<tbody>';
+    foreach ($darbiba as $ad)
+    {
+        echo '<tr>';
+        echo '<td>';
+        echo $ad->action_id;
+        echo '</td>';
+        echo '<td>';
+        echo $ad->changed_code;
+        echo '</td>';
+        echo '<td>';
+        echo $ad->changed_name;
+        echo '</td>';
+        echo '</tr>';
+    
+    }
+    echo '</tbody>';
+    echo '</table>';
+}
 
 
 ?>
