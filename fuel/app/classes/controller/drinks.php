789eb0c03b68ba4e700992d986625e265c2cfc4c
Maksim Savchuk <mixujusij@gmail.com>
2013-01-22 00:19:24 +0200

    Add displays form

--- fuel/app/classes/controller/drinks.php
+++ fuel/app/classes/controller/drinks.php
@@ -30,9 +30,21 @@ class Controller_Drinks extends Controller_Template
         $left_nav = View::forge('drinks/navigation');
         $left_nav->set('nav',$nav);
         $this->template->navigation=$left_nav;
+        $list=array();
+        $list[]=0;
         if(isset($_GET["name"])&&isset($_GET["ingr"])&&$_GET["name"]!=NULL&&$_GET["ingr"]!=NULL)
         {
-            
+            /*$list = Model_Kokteilis::find('all',array(
+                'where' => array(
+                    'related' => array(
+                        'name'=> $_GET["ingr"])
+                    )
+                )
+            );
+            $view = View::forge('drinks/list');
+            $view->set('data',$list);
+            $this->template->content=$view;
+            */
         }
         else if(isset($_GET["name"])&&$_GET["name"]!=NULL)
         {
@@ -43,7 +55,28 @@ class Controller_Drinks extends Controller_Template
         }
         else if(isset($_GET["ingr"])&&$_GET["ingr"]!=NULL)
         {
-            
+            /*
+            $ingridients = Model_Ingredient::find('all');
+           
+            $rec_id=array();
+            foreach ($ingridients as $ingr)
+            {
+                
+                
+                if($ingr->name=$_GET["ingr"])
+                {
+                    $rec_id[]=$ingr->kokteilis_id;
+                    
+                }
+                $query='';
+                foreach ($rec_id as $id)
+                {
+                    $query=$query.' id='.$id.' OR';
+                }
+            }
+            $view = View::forge('drinks/list');
+            $view->set('data',$list);
+            $this->template->content=$view;*/
         }
         else 
         {    
@@ -60,6 +93,7 @@ class Controller_Drinks extends Controller_Template
     public function  action_add()
     {
         $view=View::forge('drinks/add');
+        $this->template->content=$view;
     }
     
     public function action_about()
--- fuel/app/views/drinks/add.php
+++ fuel/app/views/drinks/add.php
@@ -1,4 +1,45 @@
 <?php
-
-
+echo 
+'<form id="coctailForm" action="http://naivist.net/form" onSubmit="return false">
+		<h3>Cocktail maker</h3>
+		<div class="inputRow" id="row1">
+		<input type="text" name="amount1" placeholder="amount" class="amount">
+		<select name="unit1" class="unit">
+			<option value="empty"></option>
+			<option value="part(s)">part</option>
+			<option value="ml">ml</option>
+			<option value="l">l</option>
+			<option value="piece">piece(s)</option>
+			<option value="oz">oz</option>
+			<option value="teaspoon">teaspoon</option>
+			<option value="dessertSpoon">dessert spoon</option>
+		</select>
+		<input name="ingredient1" type="text" placeholder="ingredient" class="ingredient">
+		</div>
+		
+		<div class="inputRow" id="row2">
+			<input name="amount2" type="text" placeholder="amount" class="amount">
+		<select name="unit2" class="unit">
+			<option value="empty"></option>
+			<option value="part(s)">part</option>
+			<option value="ml">ml</option>
+			<option value="l">l</option>
+			<option value="piece">piece(s)</option>
+			<option value="oz">oz</option>
+			<option value="teaspoon">teaspoon</option>
+			<option value="dessertSpoon">dessert spoon</option>
+		</select>
+		<input name="ingredient2" type="text" placeholder="ingredient" class="ingredient">
+		</div>
+		
+		
+		<p id="paragraphOfButtons">
+			<button id="submitButton">Submit</button>
+			<button id="add">Add ingredient</button>
+			<button id="delete">Delete ingredient</button>
+		</p>
+		 
+	
+	</form>
+';
 ?>
