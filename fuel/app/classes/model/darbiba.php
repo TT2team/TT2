5f49c4b6b3df5a577147d96432a9d33530bf2ac6
Jan Glagolev <jansglagolevs@gmail.com>
2013-01-22 00:07:48 +0200

    darbiba

--- fuel/app/classes/model/darbiba.php
+++ fuel/app/classes/model/darbiba.php
@@ -1,6 +1,6 @@
 <?php
 
-class Model_Administratora_Darbiba extends Orm\Model{
+class Model_Darbiba extends Orm\Model{
     protected static $_primary_key=array('id');
     protected static $_table_name = 'administratora_darbiba';
     protected static $_belongs_to = array('adnimistrators' => array(
