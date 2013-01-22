7850f364231e3fc48feb3dd5fb0660f6f0d0e878
Jan Glagolev <jansglagolevs@gmail.com>
2013-01-22 03:19:12 +0200

    malenjkije izmenenija

---
+++ fuel/app/config/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+return array(
+    // Драйвер/Драйверы
+    'driver' => array('SimpleAuth'),
+
+    // Включение поддержки мультивхода под одним логином
+    'verify_multiple_logins' => false,
+
+    // Ключ для хеша
+    'salt' => 'ColoredSalt',
+);
+
+
+?>
--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -30,7 +30,7 @@ return array(
    * );
    */
   'packages'  => array(
-   'orm',
+   'orm','auth',
   ),
 )
 );
